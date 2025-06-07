025c610b88a32f2daa8785860f9ef362ccd52032
Anna Ashna Joy <132273461+annaashnajoyy@users.noreply.github.com>
2025-06-07 07:12:57 +0530

    calculates area of a Triangle

---
+++ Packages/shapes/Triangle.java
@@ -0,0 +1,16 @@
+package shapes;
+
+public class Triangle {
+    double a, b, c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double area() {
+        double s = (a + b + c) / 2;
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+}
